1cb66a36cd8d7938d0a813583f0567578d51bfd5
Jonasvelle <jonasvelle@hotmail.com>
2023-10-26 21:19:08 +0200

    added test file

--- test_empty_list.py
+++ test_empty_list.py
@@ -1,3 +1,14 @@
 import pytest
-
 from main import isLeapYear
+
+@pytest.fixture(params=[2000, 2004, 2028])
+def leap_years(request):
+    return request.param
+
+
+def test_leap_years(leap_years):
+    result = isLeapYear(leap_years)
+    assert result == True, f"Year {leap_years} should be a leap year."
+
+if __name__== "main":
+    pytest.main()
